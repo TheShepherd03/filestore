c4a75597a929d0a6a9cf20919451cdd85fca0459
TheShepherd03 <sakhybig@gmail.com>
2024-08-04 22:35:38 +0200

    implemented organizations using clerk

--- filestore/convex/files.ts
+++ filestore/convex/files.ts
@@ -1,6 +1,6 @@
 
-import {v} from "convex/values";
-import {mutation} from "./_generated/server";
+import {ConvexError, v} from "convex/values";
+import {mutation, query} from "./_generated/server";
 
 
 export const createFile=mutation({
@@ -8,8 +8,24 @@ export const createFile=mutation({
         fileName:v.string(),
     },
     async handler(ctx,args){
+        const identity=await ctx.auth.getUserIdentity();
+        console.log('Your address is ',identity?.address);
+        console.log('Your phoneNumber is ',identity?.phoneNumber)
+        console.log('Your name is ',identity?.name)
+        if(!identity){
+            throw new ConvexError("Log in to perform this functionality");
+        }
         await ctx.db.insert("files",{
             fileName:args.fileName,
         });
     },
 });
+
+export const getFiles=query(
+    {
+        args:{},
+        async handler(ctx,args){
+            return ctx.db.query('files').collect()
+        }
+    }
+)
--- filestore/src/app/page.tsx
+++ filestore/src/app/page.tsx
@@ -2,22 +2,17 @@
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
 import { api } from "../../convex/_generated/api";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Home() {
   const createFile=useMutation(api.files.createFile);
+  const files=useQuery(api.files.getFiles);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedOut>
       <SignInButton mode="modal">
         <Button>Sign In</Button>
       </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-      <SignOutButton>
-        <Button>Sign Out</Button>
-      </SignOutButton>
       <Button onClick={
           ()=>{
             createFile(
@@ -25,6 +20,21 @@ export default function Home() {
             )
           }
         }>create a File</Button>
+      </SignedOut>
+
+      <SignedIn>
+      <SignOutButton>
+        <Button>Sign Out</Button>
+      </SignOutButton>
+      
+
+        {
+          files?.map((x)=>{
+            return <div key={x._id}>
+              {x.fileName}
+            </div>
+          })
+        }
       </SignedIn>
       
     </main>
